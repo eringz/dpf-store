e0b866dc690dadf4f6ac50bca56dae9429def172
E-ringz <ron.garcia.santos@gmail.com>
2022-12-23 21:36:47 +0800

    update

---
+++ application/models/Order.php
@@ -0,0 +1,28 @@
+<?php
+    defined('BASEPATH') OR exit('No direct script access allowed');
+
+    class Order extends CI_Model
+    {
+        
+        /*
+            DOCU: This function retrieve all stored orders.
+            OWNER: Ron Garcia Santos
+        */
+
+
+        /*
+            DOCU: This function adds and stores a new orders.
+            OWNER: Ron Garcia Santos
+        */
+
+        /*
+            DOCU: This function update stored orders.
+            OWNER: Ron Garcia Santos
+        */
+
+        /*
+            DOCU: This funtion destroy stored orders.
+            OWNER: Ron Garcia Santos
+        */
+    }
+?>
