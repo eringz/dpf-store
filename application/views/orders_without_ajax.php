66176352587ecf738a07eb98917e0c4711762901
E-ringz <ron.garcia.santos@gmail.com>
2022-12-23 21:24:19 +0800

    update

--- application/views/orders_without_ajax.php
+++ application/views/orders_without_ajax.php
@@ -9,5 +9,21 @@
 </head>
 <body>
 	<h1>Order Qeue</h1>
+	<div>
+		<form action="">
+			<h2>No.</h2>
+			<input type="submit" value="X">
+		</form>
+		<form action="">
+			<textarea name="" id="" cols="30" rows="10"></textarea>
+			<input type="submit" value="update">
+		</form>
+	</div>
+	<div>
+		<form action="">
+			<input type="text" name="order">
+			<input type="submit" value="Order">
+		</form>
+	</div>
 </body>
 </html>
