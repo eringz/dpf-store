81be09501bef433ae6b6c802e668a3a915609fd6
E-ringz <ron.garcia.santos@gmail.com>
2022-12-23 22:12:32 +0800

    update

--- application/views/orders_without_ajax.php
+++ application/views/orders_without_ajax.php
@@ -9,20 +9,23 @@
 </head>
 <body>
 	<h1>Order Qeue</h1>
-	<div>
-		<form action="">
-			<h2>No.</h2>
-			<input type="submit" value="X">
-		</form>
-		<form action="">
-			<textarea name="" id="" cols="30" rows="10"></textarea>
-			<input type="submit" value="update">
-		</form>
+	<div class="orders-div">
+		<div class="order-div">
+			<form action="">
+				<h2>No.</h2>
+				<input type="submit" class="remove-btn" value="X">
+			</form>
+			<form action="">
+				<textarea name="" id="" cols="35" rows="10">1 tea 1 beef</textarea>
+				<input type="submit" class="update-btn" value="update">
+			</form>
+		</div>
 	</div>
-	<div>
-		<form action="">
-			<input type="text" name="order">
-			<input type="submit" value="Order">
+
+	<div class="orders-div">
+		<form class="add-order-frm" action="">
+			<input type="text" id="add-order-input" name="order">
+			<input type="submit" id="add-order-btn" value="Order">
 		</form>
 	</div>
 </body>
